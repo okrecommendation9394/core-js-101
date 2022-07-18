c387ef5a73c8e9904d35ff5074d8f8f6c1b6399f
okrecommendation9394 <qetigogia50@gmail.com>
2022-07-18 06:52:20 -0700

    feat: fixing linter bug

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -18,8 +18,8 @@
  *   'aa',''    => 'aa'
  *   '',  'bb'  => 'bb'
  */
-function concatenateStrings(value1, value2 ) {
-  return value1+value2;
+function concatenateStrings(value1, value2) {
+  return value1 + value2;
 }
 
 
