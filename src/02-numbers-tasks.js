51eabf2ffddb3168103109a7bd8a118d39f6d051
okrecommendation9394 <qetigogia50@gmail.com>
2022-07-20 02:20:30 -0700

    feat: solved last task

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -234,8 +234,13 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  if (value instanceof Number) {
+    return +value;
+  } if (+value) {
+    return +value;
+  }
+  return def;
 }
 
 module.exports = {
