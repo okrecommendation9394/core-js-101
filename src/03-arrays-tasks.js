afec2ff9df869d025e680fc6dfaabe6a9389b9b0
okrecommendation9394 <qetigogia50@gmail.com>
2022-07-20 08:20:42 -0700

    feat: solved getPositivesCount

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -312,8 +312,14 @@ function get3TopItems(arr) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  let num = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] > 0 && typeof arr[i] === 'number') {
+      num += 1;
+    }
+  }
+  return num;
 }
 
 /**
