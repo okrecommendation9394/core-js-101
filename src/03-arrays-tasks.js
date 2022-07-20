521e66249fc29d41563960b669f0ad1439c69eb8
okrecommendation9394 <qetigogia50@gmail.com>
2022-07-20 07:51:09 -0700

    feat: solved propagateItems

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -268,8 +268,14 @@ function getSecondItems(arr) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  const array = [];
+  arr.forEach((x, i) => {
+    for (let j = 0; j < i + 1; j += 1) {
+      array.push(x);
+    }
+  });
+  return array;
 }
 
 
