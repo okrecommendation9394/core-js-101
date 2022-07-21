4f51458a129335d80c5bfa5596b4c99aab950bd8
okrecommendation9394 <qetigogia50@gmail.com>
2022-07-21 07:19:27 -0700

    feat: solved generateOdds

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -35,8 +35,12 @@ function findElement(arr, value) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const arr = [];
+  for (let i = 1; i < len * 2; i += 2) {
+    arr.push(i);
+  }
+  return arr;
 }
 
 
