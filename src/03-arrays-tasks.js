fef0ef2e2b22e7cea8dee5960696f0c0b3317f38
okrecommendation9394 <qetigogia50@gmail.com>
2022-07-21 07:11:00 -0700

    feat: solved doubleArray

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -52,8 +52,11 @@ function generateOdds(/* len */) {
  *    [0, 1, 2, 3, 4, 5] => [0, 1, 2, 3, 4, 5,   0, 1, 2, 3, 4, 5]
  *    [] => []
  */
-function doubleArray(/* arr */) {
-  throw new Error('Not implemented');
+function doubleArray(arr) {
+  arr.forEach((elem) => {
+    arr.push(elem);
+  });
+  return arr;
 }
 
 
