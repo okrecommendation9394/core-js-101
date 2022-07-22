d13610f90e8672782565e947cb64c8463baa677c
okrecommendation9394 <qetigogia50@gmail.com>
2022-07-22 07:30:33 -0700

    feat: solved angles

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -101,8 +101,14 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const hoursDegree = 30 * hours;
+  const minutesDegree = 6 * minutes;
+  const degree = hoursDegree > minutesDegree ? hoursDegree - minutesDegree
+    : minutesDegree - hoursDegree;
+  return (degree * Math.PI) / 180;
 }
 
 
