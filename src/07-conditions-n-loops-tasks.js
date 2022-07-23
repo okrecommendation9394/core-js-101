20eda2effe1a7ba4e1525d5dc45e92f7d307c131
okrecommendation9394 <qetigogia50@gmail.com>
2022-07-23 11:20:29 -0700

    feat: creditCard

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -312,8 +312,23 @@ function reverseInteger(num) {
  *   5436468789016589 => false
  *   4916123456789012 => false
  */
-function isCreditCardNumber(/* ccn */) {
-  throw new Error('Not implemented');
+function isCreditCardNumber(ccn) {
+  const digits = ccn.toString().split('').map(Number);
+  const sum = digits
+    .map((digit, index) => {
+      if (index % 2 === digits.length % 2) {
+        if (digit * 2 > 9) {
+          return digit * 2 - 9;
+        }
+        return digit * 2;
+      }
+      return digit;
+    });
+  let acc = 0;
+  for (let i = 0; i < sum.length; i += 1) {
+    acc += sum[i];
+  }
+  return acc % 10 === 0;
 }
 
 /**
