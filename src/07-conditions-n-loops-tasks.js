8d3a71952bc2e639661cc8f089f06197048a6816
okrecommendation9394 <qetigogia50@gmail.com>
2022-07-23 07:31:45 -0700

    feat: solved rectangles

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -139,8 +139,14 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  if (rect1.left > rect2.left + rect2.width
+    || rect1.left + rect1.width < rect2.left
+    || rect1.top > rect2.top + rect2.height
+    || rect1.top + rect1.height < rect2.top) {
+    return false;
+  }
+  return true;
 }
 
 
