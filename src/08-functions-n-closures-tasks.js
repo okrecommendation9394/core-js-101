2da17933d208cbbae70f7a9d8e5458a58a7b31c8
okrecommendation9394 <qetigogia50@gmail.com>
2022-07-25 05:54:41 -0700

    feat: solved memoize

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -92,8 +92,9 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const number = func();
+  return () => number;
 }
 
 
